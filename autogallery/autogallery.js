1041e24e228063ef89befb2e6118dfce3ddf3b2d
Timothy Austen <dfdfdfcvcvcv@gmail.com>
2016-06-07 08:14:53 +0300

    Add css media query to header

--- autogallery/autogallery.js
+++ autogallery/autogallery.js
@@ -239,7 +239,7 @@ function autogalleryfunc(fileList, dir, fileextension, galleryContainer, maxwidt
         
         $('#' + galleryContainer).prepend($(
             '<div id="fullscreenBackground"></div>' +
-            '<div id="fullscreenButton">Fullscreen</div>'
+            '<div id="fullscreenButton">[ ]</div>'
         ));
 
         $('#fullscreenButton').css({
@@ -400,6 +400,20 @@ function autogalleryfunc(fileList, dir, fileextension, galleryContainer, maxwidt
                 dimensions = new EstimatedHeight(galleryContainer);
                 dimensions.resize;
                 
+                // Exit fullscreen
+                function exitFullscreen() {
+                    if(document.exitFullscreen) {
+                        document.exitFullscreen();
+                    } else if(document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if(document.webkitExitFullscreen) {
+                        document.webkitExitFullscreen();
+                    }
+                }
+
+                // Exit fullscreen for browsers that support it!
+                exitFullscreen();
+
                 $('#fullscreenButton').html('[ ]');
                 fullscreenIsOn = false;
                 // bookmark
